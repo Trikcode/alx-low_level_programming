80b05f34ccd74e7eb0811a6a7e555da88ecf8ada
Wise2525 <ayesiganobert@gmail.com>
2021-06-17 05:16:53 -0700

    u can still count 🎁🎁🎁👓👓👓

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
- * main - print string
- * Description: don't use printf or puts
+ * main - prints to string
+ *
+ * Description: Prints "and that piece of art is useful..." without puts
+ *
  * Return: 1
  */
 int main(void)
 {
-	char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-
-	__asm__ ("syscall"
-	: "=a" (ret)
-	: "a" (syscall),
-	"D" (fd),
-	"S" (s),
-	"d" (1));
-	return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
