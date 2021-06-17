600f7a747944901f69136f47d2289c3a116f12d9
Wise2525 <ayesiganobert@gmail.com>
2021-06-17 04:33:02 -0700

    using printf⛷⛷⛷⛷⛷⛷⛷⛷

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
