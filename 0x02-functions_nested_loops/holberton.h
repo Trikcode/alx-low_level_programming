0d73dfc65c5190d96b1b545b512f365f9e3515fd
Wise2525 <ayesiganobert@gmail.com>
2021-06-22 08:11:00 -0700

    24 hours🕊🕊🕊

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,7 +10,13 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 
 int _islower(int c);
+
 int _isalpha(int n);
+
 int print_sign(int n);
+
 int _abs(int n);
+
 int print_last_digit(int n);
+
+void jack_bauer(void);
