ab8845896ab638f71139c99bb9648215b18e150b
Wise2525 <ayesiganobert@gmail.com>
2021-06-22 07:20:41 -0700

    fivr sign🦜🦜🦜

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,26 @@
+
 #include "holberton.h"
 
 /**
- * print_sign - check numers
- * @n : number to check
- * Return:0 or 1
+ * print_sign - prints the sign of a number
+ * @n: interger to be tested
+ * Return: 1, 0, -1
  */
-
 int print_sign(int n)
 {
-int test;
-
-if (n > 0)
-{
-	test = 1;
-	_putchar('+');
-}
-else if (n == 0)
-{
-	test = 0;
-	_putchar('0');
-}
-else
-{
-	test = -1;
-	_putchar('-');
-}
-
-return (test);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
