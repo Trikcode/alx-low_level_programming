036d13fee81ddb66a317d043a72044732adc7c5a
Wise2525 <ayesiganobert@gmail.com>
2021-06-21 20:28:44 -0700

    alphabets🦜🦜🦜

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton"
+/**
+ * print_alphabet - Entry block
+ * Description: print alphabet in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
