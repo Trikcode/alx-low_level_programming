738bade4de69e6aad54f2441a87ccdfda8b12378
Wise2525 <ayesiganobert@gmail.com>
2021-06-22 04:44:50 -0700

    holberton file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,17 @@
-#ifindef HOLBERTON_H
 #define HOLBERTON_H
 
 /* _putchar.c */
 int _putchar(char);
+
+/*10 x alphabet */
+void print_alphabet_x10(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int c);
+
+/* 4-isalpha.c  */
+int _isalpha(int c);
+
