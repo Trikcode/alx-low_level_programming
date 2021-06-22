19be3c2c47c799912183b4a23103955167fee69c
Wise2525 <ayesiganobert@gmail.com>
2021-06-22 06:54:22 -0700

    h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,32 +9,3 @@ void print_alphabet(void);
 /* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 
-/* 3-islower.c */
-int _islower(int c);
-
-/* 4-isalpha.c  */
-int _isalpha(int c);
-
-/* 5-sign.c */
-int print_sign(int n);
-
-/* 6-abs.c */
-int _abs(int n);
-
-/* 7-print_last_digit.c */
-int print_last_digit(int);
-
-/* 8-24_hours.c */
-void jack_bauer(void);
-
-/* 9 - timestable.c */
-void times_table(void);
-
-/* 10 - add.c */
-int add(int, int);
-
-/* 11-print_to_98.c */
-void print_to_98(int n);
-
-/* 100-times_table.c */;
-void print_times_table(int);
