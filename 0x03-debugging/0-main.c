59ee350148d07326929ebebe0f746974bcd04cb0
Wise2525 <ayesiganobert@gmail.com>
2021-06-23 21:54:47 -0700

    main🕊🕊

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - Test function for positive or negative
+ * main - Test  function for positive or negative
  * Return: 0
  */
 
@@ -10,6 +10,7 @@ int main(void)
 int i;
 
 i = 0;
+
 positive_or_negative(i);
 return (0);
 }
