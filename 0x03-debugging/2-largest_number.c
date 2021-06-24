d6eb18a4d56d8f398b0c8a1e26a2a6677c55e445
Wise2525 <ayesiganobert@gmail.com>
2021-06-23 20:49:53 -0700

    largest🕊🕊🕊🕊

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,10 +24,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else
-	{
-		largest = b;
-	}
-
 	return (largest);
 }
--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,11 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
-
-int largest_number(int a, int b, int c);
-
+void positive_or_negative(int);
+int largest_number(int, int, int);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
