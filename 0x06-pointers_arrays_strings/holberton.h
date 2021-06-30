fd643092a9521033540b18ad1acfd1330149c31d
Wise2525 <ayesiganobert@gmail.com>
2021-06-30 03:32:08 -0700

    letters to digits✨✨✨

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,3 +4,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
