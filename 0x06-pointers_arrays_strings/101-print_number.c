7537e6c3ed58101308f19b837bc5d9653ff3752f
Wise2525 <ayesiganobert@gmail.com>
2021-07-01 04:52:07 -0700

    print numbers✨✨✨

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,26 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an interger.
- * @n: interger.
- * Return: nothing.
- */
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+unsigned int n1;
+
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+
+if (n1 / 10 != 0)
+{
+		print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
