9bd007729fdaba6448e369ac83d913a7bf2d5961
Wise2525 <ayesiganobert@gmail.com>
2021-07-01 05:05:35 -0700

    compare✨✨✨

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
+
 /**
  * _strcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
-*/
-
+ * Return: 1 if true, 0 if false
+ */
 int _strcmp(char *s1, char *s2)
 {
-int i;
+	int equal = 0;
 
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-	if (s1[i] != s2[i])
+	while (*s1)
 	{
-		return (s1[i] - s2[i]);
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
 	}
-}
-return (0);
+	return (equal);
 }
