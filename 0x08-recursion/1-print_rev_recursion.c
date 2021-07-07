30ee7dc5a6d0c7af756a1bdf53bf580d57a3805b
Wise2525 <ayesiganobert@gmail.com>
2021-07-07 04:17:45 -0700

    reverse🍿🍿🍿

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to string
+ * *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
