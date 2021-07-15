a9e81aadd7eb476d96a0cc331dd4082ba5094858
Wise2525 <ayesiganobert@gmail.com>
2021-07-15 12:58:45 -0700

    array🕊🕊🕊

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,24 @@
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * *array_range -  create an array of integers
- * @min: size of the array
- * @max: char to initialize
- * Return: pointer to the array initialized or NULL
+ * array_range - creates an array of integers
+ * @min: starting int
+ * @max: max int
+ * Return: array of integers
  */
-
 int *array_range(int min, int max)
 {
-int *m;
-int i;
+	int len, i;
+	int *ptr;
 
-if (min > max)
-	return (NULL);
-
-m = malloc((max - min + 1) * sizeof(int));
-
-if (m == 0)
-	return (NULL);
-
-
-for (i = 0; min + i <= max; i++)
-	m[i] = min + i;
-
-return (m);
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+	ptr = malloc(sizeof(int) * len);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ptr[i] = min++;
+	return (ptr);
 }
