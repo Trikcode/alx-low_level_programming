23ee2356c8f70c999b7c03a7b20a4f74783e031a
Wise2525 <ayesiganobert@gmail.com>
2021-07-15 03:26:24 -0700

    multiply🕊🕊🕊

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
