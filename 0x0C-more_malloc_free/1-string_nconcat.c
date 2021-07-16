52685d9cf399738bf740cdd8a12a534e5b8a8e44
Wise2525 <ayesiganobert@gmail.com>
2021-07-16 00:12:40 -0700

    conc🕊🕊🕊

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
-	strDup = malloc(sizeof(char) * (i + 1));
+	strDup = malloc(sizeof(char) * (i + n + 2));
 	if (strDup == NULL)
 		return (NULL);
 	i = j = 0;
