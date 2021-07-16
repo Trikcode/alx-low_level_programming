4c6b0125dd27e3fcd38c007eebe5f643b7984ccf
Wise2525 <ayesiganobert@gmail.com>
2021-07-16 01:44:50 -0700

    strin

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,48 @@
 #include "holberton.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
  */
-
-unsigned int _strlen(char *s)
+int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
+	unsigned int i, j, size, len1, len2;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n > len2)
+		n = len2;
+	size = len1 + n;
+	ptr = malloc(sizeof(char) * size + 1);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		ptr[i] = s2[j];
+	ptr[i] = 0;
+	return (ptr);
 }
 
 /**
- * *string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: first bytes of s2 to be used
- * Return: pointer or NULL
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+int _strlen(char *s)
 {
-unsigned int i, j;
-char *m;
+	char *p = s;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-
-if (n < _strlen(s2))
-	m = malloc(_strlen(s1) + sizeof(char) * (n + 1));
-else
-	m = malloc(_strlen(s1) + _strlen(s2) + 1);
-n = 0;
-if (m == NULL)
-	return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-	m[n++] = s1[i];
-
-for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-	m[n++] = s2[j];
-
-m[n] = '\0';
-
-return (m);
+	while (*s)
+		s++;
+	return (s - p);
 }
