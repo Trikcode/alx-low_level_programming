9fd148a80fb768d7b3d17c096c67381b00ac4ee5
Wise2525 <ayesiganobert@gmail.com>
2021-07-19 06:54:20 -0700

    initialize🕊🕊🕊

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
