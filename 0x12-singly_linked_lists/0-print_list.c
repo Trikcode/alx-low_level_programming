cd329f96eca1ac05e26d91d371c889b3744882d2
Wise2525 <ayesiganobert@gmail.com>
2021-07-29 06:35:25 -0700

    printlist🕊🕊🕊

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print elements of linkedlist
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nbr_nodes = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("[%d] %s\n", current->len,
+		current->str != NULL ? current->str : "(nil)");
+		current = current->next;
+		nbr_nodes++;
+	}
+	return (nbr_nodes);
+}
