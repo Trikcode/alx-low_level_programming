b958e82956d426583c9940575322f0583e57d0b2
Wise2525 <ayesiganobert@gmail.com>
2021-07-29 07:32:54 -0700

    assembly🕊🕊🕊

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,15 @@
-0x0000000000001149 <+0>:     endbr64
-0x000000000000114d <+4>:     push   %rbp
-0x000000000000114e <+5>:     mov    %rsp,%rbp
-0x0000000000001151 <+8>:     lea    0xeac(%rip),%rdi        # 0x2004
-0x0000000000001158 <+15>:    callq  0x1050 <puts@plt>
-0x000000000000115d <+20>:    mov    $0x0,%eax
-0x0000000000001162 <+25>:    pop    %rbp
-0x0000000000001163 <+26>:    retq
+global main
+	extern printf
+
+	section .text
+main:
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0
+	call printf
+	pop rbp
+	mov rax, 0
+	ret
+message:	 db "Hello, Holberton", 0
+format:		db "%s", 10, 0
