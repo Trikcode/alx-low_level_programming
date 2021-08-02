a9dc1c511c0bc81620edbbc4ece8ffe1e7b5687e
Wise2525 <ayesiganobert@gmail.com>
2021-08-01 23:01:09 -0700

    head node's data🕊🕊🕊

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: head of linked list
+ * Return: the head node’s data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int v;
+
+	if (head == NULL)
+		return (0);
+
+	current = *head;
+	if (current == NULL)
+		return (0);
+
+	v = current->n;
+	*head = current->next;
+	free(current);
+	return (v);
+}
