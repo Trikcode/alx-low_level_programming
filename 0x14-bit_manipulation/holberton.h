986a0129418e896980027de1353228cf7ad91c42
Wise2525 <ayesiganobert@gmail.com>
2021-08-05 00:06:19 -0700

    header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,5 +5,5 @@ int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
-
+int set_bit(unsigned long int *n, unsigned int index);
 #endif /* _HOLBERTON_H_ */
