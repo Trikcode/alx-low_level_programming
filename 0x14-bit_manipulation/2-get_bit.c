5ca0925bd3261c420a4a4f5477f8bcaf8a2f7a90
Wise2525 <ayesiganobert@gmail.com>
2021-08-05 00:11:04 -0700

    get bit🕊🕊🕊

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,16 @@
 #include "holberton.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
- * @n: number to check bits in
- * @index: index at which to check bit
- *
- * Return: value of the bit, or -1 if there is an error
+ * get_bit - gets bit on index `index`
+ * @n: decimal number
+ * @index: index of the bit
+ * Return: the bit found
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
-
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index >= sizeof(n) * BYTE_LENGHT)
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+
+	return ((n >> index) & 1);
 }
