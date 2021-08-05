7f07ed6cbb81bb50d25716ca580594e8bfa1cdf8
Wise2525 <ayesiganobert@gmail.com>
2021-08-05 00:18:42 -0700

    bit file🕊🕊🕊

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
- * get_bit - gets bit on index `index`
- * @n: decimal number
- * @index: index of the bit
- * Return: the bit found
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number whose bit to get
+ * @index: the index at which to return the value
+ * Return: the value of a bit at a given index
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * BYTE_LENGHT)
-		return (-1);
+	unsigned int i;
 
-	return ((n >> index) & 1);
+	for (i = 0; i < index; i++)
+		n >>= 1;
+	if (i > ((sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+	return (n & 1);
 }
