ff924d8300e95e941083581a1a5ef69b2fcf8d6d
Wise2525 <ayesiganobert@gmail.com>
2021-08-05 04:50:13 -0700

    file🕊🕊🕊

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,24 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef notrebloh_H
+#define notrebloh_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 
 int _putchar(char c);
+
 unsigned int binary_to_uint(const char *b);
+
 void print_binary(unsigned long int n);
+
 int get_bit(unsigned long int n, unsigned int index);
+
 int set_bit(unsigned long int *n, unsigned int index);
+
 int clear_bit(unsigned long int *n, unsigned int index);
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int get_endianness(void);
 
 #endif
