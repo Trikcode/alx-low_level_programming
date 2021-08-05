35e50459a952e6205f55c7b2e6a40b41f8ec62e0
Wise2525 <ayesiganobert@gmail.com>
2021-08-04 23:56:55 -0700

    header

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+
+#endif /* _HOLBERTON_H_ */
